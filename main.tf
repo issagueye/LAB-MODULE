18e6ff8c34bd6c5094e0a38596f998d20a1f7b12
Issa Gueye <issacolygueye@gmail.com>
2021-01-14 17:15:04 +0000

    Ajout Backend & access KEY & Secret Access KEY
    Ajout Backend & access KEY & Secret Access KEY

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
+#Remote Backend Configuration
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "BAOBABGROUP"
+
+    workspaces {
+      name = "LAB-MODULE"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_access_key
 }
 
 module "my_ec2_instance" {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
+variable "access_key" {
+    type = string
+}
+
+variable "secret_access_key" {
+    type = string
+}
 variable "instance_type" {
   type = string
   #default = "t2.micro"
